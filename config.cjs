60933c9031aaed4794e97352f012b0879482e632
innocentman17 <mathias17herry@gmail.com>
2025-05-28 23:54:45 +0300

    Update config.cjs
    Signed-off-by: innocentman17 <mathias17herry@gmail.com>

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Your Session Id",
+  SESSION_ID: process.env.SESSION_ID || "Your Session Id"Buddy;;;kVhUwJKK#4fI881y9ptJDmawFR_xNovKc2K9VNA639lT6NT-N3dg",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT !== undefined ? process.env.AUTO_STATUS_REACT === 'true' : true, 
